94966146f6d2db78d2cb2217751c8c7578a7d798
sqrt <sqrt@octoblu.com>
2016-07-12 17:54:48 -0700

    Start building onetimetoken exchange

--- onetimetoken/onetimetoken.go
+++ onetimetoken/onetimetoken.go
@@ -1,4 +1,58 @@
 package onetimetoken
 
-// New constructs a new instance of Token
-func New() {}
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// OTP is an interface for retrieving information about One Time Tokens and to expire them
+type OTP interface {
+	ExchangeForInformation() OTPInformation
+}
+
+type httpOTP struct {
+	oneTimePassword, urlStr string
+}
+
+// OTPInformation describes the information that a One Time Password can be exchanged for
+type OTPInformation struct {
+	UUID, Token string
+}
+
+// New constructs a new OTP instance
+func New(oneTimePassword string) OTP {
+	urlStr := "https://meshblu-otp.octoblu.com/"
+
+	return &httpOTP{oneTimePassword, urlStr}
+}
+
+// NewWithURLOverride constructs a new OTP instance with a specific URL
+func NewWithURLOverride(oneTimePassword, urlStr string) OTP {
+	return &httpOTP{oneTimePassword, urlStr}
+}
+
+// ExchangeForInformation exchanges a one time token for information, including
+// the connector type and Meshblu credentials
+func (otp *httpOTP) ExchangeForInformation() OTPInformation {
+	retrievalURL, _ := url.Parse(otp.urlStr)
+	retrievalURL.Path = fmt.Sprintf("/retrieve/%v", otp.oneTimePassword)
+
+	response, _ := http.Get(retrievalURL.String())
+	return parseRetrievalResponse(response)
+}
+
+func parseRetrievalResponse(response *http.Response) OTPInformation {
+	return OTPInformation{}
+}
+
+// uuid: 'c7097087-bed4-4272-8692-3b07277ec281',
+// token: 'a7702204e157e51fd63c924a7b77db00121a0d5d',
+// metadata: {
+//   githubSlug: 'octoblu/meshblu-connector-say-hello',
+//   connectorAssemblerVersion: 'v13.0.0',
+//   dependencyManagerVersion: 'v3.0.2',
+//   ignitionVersion: 'v6.0.0',
+//   connector: 'say-hello',
+//   tag: 'v6.0.0',
+// },
--- onetimetoken/onetimetoken_test.go
+++ onetimetoken/onetimetoken_test.go
@@ -1,16 +1,87 @@
 package onetimetoken_test
 
 import (
+	"net/http"
+	"net/http/httptest"
+
 	"github.com/octoblu/go-meshblu-connector-installer/onetimetoken"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Onetimetoken", func() {
-	Describe("constructor", func() {
-		It("Should be a function", func() {
-			Expect(onetimetoken.New).NotTo(BeNil())
+var _ = Describe("onetimetoken", func() {
+	var sut onetimetoken.OTP
+	var server *httptest.Server
+	var lastRequest *http.Request
+	var nextResponseBody string
+	var nextResponseStatus int
+
+	BeforeEach(func() {
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			lastRequest = r
+
+			w.Header().Add("Content-Type", "applicaton/json")
+			w.WriteHeader(nextResponseStatus)
+			w.Write([]byte(nextResponseBody))
+		}))
+	})
+
+	AfterEach(func() {
+		server.Close()
+	})
+
+	Describe("->New", func() {
+		It("should produce an instance", func() {
+			Expect(onetimetoken.New("otp")).NotTo(BeNil())
+		})
+	})
+
+	Describe("->NewWithURLOverride", func() {
+		BeforeEach(func() {
+			sut = onetimetoken.NewWithURLOverride("otp", server.URL)
+		})
+
+		It("should produce an instance", func() {
+			Expect(sut).NotTo(BeNil())
+		})
+	})
+
+	Describe("with an instance", func() {
+		BeforeEach(func() {
+			sut = onetimetoken.NewWithURLOverride("otp", server.URL)
+		})
+
+		Describe("sut.ExchangeForInformation", func() {
+			Describe("when called", func() {
+				BeforeEach(func() {
+					sut.ExchangeForInformation()
+				})
+
+				It("Should call GET /retrieve/otp on the server", func() {
+					Expect(lastRequest).NotTo(BeNil())
+					Expect(lastRequest.Method).To(Equal("GET"))
+					Expect(lastRequest.URL.Path).To(Equal("/retrieve/otp"))
+				})
+			})
+
+			Describe("when the server yields a response", func() {
+				var info onetimetoken.OTPInformation
+
+				BeforeEach(func() {
+					nextResponseStatus = 200
+					nextResponseBody = `{"uuid":"some-uuid","token":"some-token"}`
+					info = sut.ExchangeForInformation()
+				})
+
+				It("Return the uuid", func() {
+					Expect(info.UUID).To(Equal("some-uuid"))
+				})
+
+				It("Return the token", func() {
+					Expect(info.Token).To(Equal("some-token"))
+				})
+			})
 		})
 	})
 })
